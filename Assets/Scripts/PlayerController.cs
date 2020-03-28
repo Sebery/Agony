3b457864395ad5d86dda390c71b31bc32a7b0bfc
Sebery <62361289+Sebery@users.noreply.github.com>
2020-03-27 22:14:44 -0600

    0.1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Components")]
+    private Animator anim;
+    private Rigidbody2D rbPlayer;
+    public float speed;
+    private float xPos;
+    private float yPos;
+    private bool moving;
+    private float lastMoveX;
+    private float lastMoveY;
+
+
+
+    private void Awake() {
+        rbPlayer = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate() {
+        PlayerMovement();
+    }
+
+    public void PlayerMovement() {
+        xPos = Input.GetAxisRaw("Horizontal");
+        yPos = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("xPos", xPos);
+
+        if (xPos != 0 || yPos != 0) {
+            if (!moving) {
+                moving = true;
+                anim.SetBool("Moving", moving);
+                lastMoveX = xPos;
+                lastMoveY = yPos;
+                anim.SetFloat("LastMoveX", lastMoveX);
+            }
+
+        } else {
+            if (moving) {
+                moving = false;
+                anim.SetBool("Moving", moving);
+            }
+
+        }
+
+        rbPlayer.velocity = new Vector2(xPos, yPos) * speed;
+    }
+
+
+}
